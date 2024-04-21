610aefe8e5ba2128c202d20f03a46b2ce82e1c2a
Akash-8442 <411akashchaurasia@gmail.com>
2024-04-21 11:55:10 +0530

    lil change - fx818

--- component/views.py
+++ component/views.py
@@ -75,11 +75,11 @@ def login(request):
         rows = cursor.fetchall()
         res = rows
 
-        global name
-        name = res[0][0]
 
         if len(res)!=0 and res[0][-1] == password:
 
+            global name
+            name = res[0][0]
             global universal_username
             universal_username = (username,)
 
